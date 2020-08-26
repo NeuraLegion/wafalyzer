9f1cb3d8e1d7e7388e5556f3bf3df9ff0a28afc0
Sijawusz Pur Rahnama <sija@sija.pl>
2020-08-27 00:44:40 +0200

    Add basic detection functionality to the CLI

--- src/cli.cr
+++ src/cli.cr
@@ -1,15 +1,39 @@
 require "option_parser"
+require "climate"
 require "./wafalyzer"
 
+Climate.configure do |settings|
+  settings.use_defaults!
+end
+
+Colorize.on_tty_only!
+
 OptionParser.parse do |parser|
-  parser.banner = "Usage: wafalyzer [arguments]"
+  parser.banner = "Usage: {#{PROGRAM_NAME}} <url> [arguments]"
 
   parser.on "-v", "--version", "Shows version" do
     puts Wafalyzer::VERSION
     exit
   end
   parser.on "-h", "--help", "Shows help" do
-    puts parser
+    puts parser.to_s.climatize
     exit
   end
+end
+
+url = ARGV[0]?
+url ||
+  abort "No <url> given!".climatize
+
+wafs = Wafalyzer.detect(url)
+
+case wafs.size
+when 0
+  puts "No WAF detected"
+when 1
+  puts "Detected WAF: %s" % \
+    wafs.first.product.colorize(:green)
+else
+  puts "Detected more than one (!) WAF: %s" % \
+    wafs.join(", ", &.product.colorize(:green))
 end
