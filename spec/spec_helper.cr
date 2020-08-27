386883a53e760b2e2ba9fa44889e4ee7a3d93fe1
Sijawusz Pur Rahnama <sija@sija.pl>
2020-08-27 14:19:16 +0200

    Use Spectator shard for specs

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,9 @@
-require "spec"
+require "spectator"
+require "spectator/should"
 require "../src/wafalyzer"
+
+Spectator.configure do |config|
+  config.fail_blank
+  config.randomize
+  config.profile
+end
--- spec/wafalyzer_spec.cr
+++ spec/wafalyzer_spec.cr
@@ -1,9 +1,1 @@
 require "./spec_helper"
-
-describe Wafalyzer do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
-  end
-end
