1a61737a67825961a477e673418f742de120361f
Sijawusz Pur Rahnama <sija@sija.pl>
2020-08-30 01:24:35 +0200

    Pass the CLI arguments into the Wafalyzer.detect call

--- src/cli.cr
+++ src/cli.cr
@@ -8,8 +8,27 @@ end
 
 Colorize.on_tty_only!
 
+method = "GET"
+body = nil
+
+use_random_user_agent = false
+user_agent = nil
+
 OptionParser.parse do |parser|
   parser.banner = "Usage: {#{PROGRAM_NAME}} <url> [arguments]"
+
+  parser.on "-m VALUE", "--method=VALUE", "Uses supplied method type when issuing request" do |value|
+    method = value.upcase
+  end
+  parser.on "-b VALUE", "--body=VALUE", "Uses supplied body when issuing request" do |value|
+    body = value
+  end
+  parser.on "-r", "--random-user-agent", "Uses a random user agent string for the issued HTTP requests" do
+    use_random_user_agent = true
+  end
+  parser.on "-u VALUE", "--user-agent=VALUE", "Uses supplied user agent string" do |value|
+    user_agent = value
+  end
 
   parser.on "-v", "--version", "Shows version" do
     puts Wafalyzer::VERSION
@@ -25,7 +44,16 @@ url = ARGV[0]?
 url ||
   abort "No <url> given!".climatize
 
-wafs = Wafalyzer.detect(url)
+Wafalyzer.configure do |settings|
+  settings.use_random_user_agent = use_random_user_agent
+end
+
+wafs = Wafalyzer.detect(
+  url: url,
+  method: method,
+  body: body,
+  user_agent: user_agent,
+)
 
 case wafs.size
 when 0
