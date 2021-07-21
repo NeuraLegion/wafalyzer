a79c34d43bfd0331595e52c6e322470ca4b45b38
Anton Strilez <as@evait.de>
2021-07-21 13:03:03 +0200

    Add option to disable ssl verification (#5)
    * add disable option for ssl verifications
    
    * Update src/wafalyzer.cr
    
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>
    
    * Update src/cli.cr
    
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>
    
    * Update src/cli.cr
    
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>
    
    * Update src/wafalyzer/settings.cr
    
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>
    
    * Update src/cli.cr
    
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>
    
    * Update src/wafalyzer.cr
    
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>
    
    * Update src/cli.cr
    
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>
    
    Co-authored-by: Paul Werther <pw@evait.de>
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>

--- src/wafalyzer.cr
+++ src/wafalyzer.cr
@@ -21,6 +21,7 @@ module Wafalyzer
       port: port,
       tls: tls,
     )
+    client.tls?.try(&.verify_mode = :none) if settings.disable_ssl_verification?
 
     if timeout = settings.timeout
       client
