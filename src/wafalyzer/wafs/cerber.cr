e74a15fca2356ea25d4300a04eb5f1761019f676
Bar Hofesh <bar.hofesh@gmail.com>
2022-06-30 11:58:00 +0300

    format

--- src/wafalyzer/wafs/cerber.cr
+++ src/wafalyzer/wafs/cerber.cr
@@ -3,10 +3,9 @@ module Wafalyzer
     register product: "Cerber (WordPress)"
 
     PATTERN = Regex.union([
-    /We're sorry, you are not allowed to proceed\<\/h1\>/,
-    /\<p\>Your request looks suspiciously similar to automated requests from spam posting software or it has been denied by a security policy configured by the website administrator\.\<\/p\>/
+      /We're sorry, you are not allowed to proceed\<\/h1\>/,
+      /\<p\>Your request looks suspiciously similar to automated requests from spam posting software or it has been denied by a security policy configured by the website administrator\.\<\/p\>/,
     ])
-
 
     builder do
       matches_body PATTERN
