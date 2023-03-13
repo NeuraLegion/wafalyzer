39b729935fd6215c93065fcc99dba4cfa4f2c645
Bar Hofesh <bar.hofesh@gmail.com>
2023-03-13 10:12:33 +0200

    Remove false indication of WAF
    This line will match the word ngnix, this is in no way an indication that a WAF is active on the target.

--- src/wafalyzer/wafs/nginx.cr
+++ src/wafalyzer/wafs/nginx.cr
@@ -2,11 +2,7 @@ module Wafalyzer
   class Waf::Nginx < Waf
     register product: "Nginx Generic Protection"
 
-    PATTERN =
-      Regex.union(
-        /nginx/i,
-        /you.do(not|n.t)?.have.permission.to.access.this.document/,
-      )
+    PATTERN = /you.do(not|n.t)?.have.permission.to.access.this.document/
 
     builder do
       matches_body PATTERN
